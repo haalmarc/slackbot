3a008c12b980eb5b0a0abbc9e2076159dd0455c0
Marcus <marcus.haaland@bekk.no>
2022-04-09 19:29:55 +0200

    conditionally run bolt app and use env port

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ const VERIFY_NEW_URL = false;
   For lokal kjøring med ngrok lages en url som peker på en port. Porten brukt i bolt-app 
   må derfor være lik den som ble satt opp med ngrok.
 */
-const PORT = VERIFY_NEW_URL ? 3001 : 3000;
+const PORT = process.env.PORT || 3000;
 
 if (VERIFY_NEW_URL) {
   verifyNewUrl();
+} else {
+  // Boten trenger noen hemmelige credentials for å få tilgang til Slack API.
+  const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET, // Hent SigningSecret fra Basic Information
+    token: process.env.SLACK_BOT_TOKEN, // Hent Bot User OAuth Token fra OAuth & Permissions
+  });
+
+  (async () => {
+    await app.start(PORT);
+
+    console.log(`Bolt-app kjører på port ${PORT}`);
+  })();
+
+  // Boten lytter til eventet melding med innholdet "heisann"
+  app.message("heisann", async ({ message, say }) => {
+    // say() sender en melding til kanalen hvor eventet ble avfyrt.
+    await say(`Halla på 'ræ <@${message.user}>!`);
+  });
 }
-
-// Boten trenger noen hemmelige credentials for å få tilgang til Slack API.
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET, // Hent SigningSecret fra Basic Information
-  token: process.env.SLACK_BOT_TOKEN, // Hent Bot User OAuth Token fra OAuth & Permissions
-});
-
-(async () => {
-  await app.start(PORT);
-
-  console.log(`Bolt-app kjører på port ${PORT}`);
-})();
-
-// Boten lytter til eventet melding med innholdet "heisann"
-app.message("heisann", async ({ message, say }) => {
-  // say() sender en melding til kanalen hvor eventet ble avfyrt.
-  await say(`Halla på 'ræ <@${message.user}>!`);
-});
 
 /*
   Slack trenger å verifisere at du er eier av URL-en. Dette trengs bare å sjekkes
@@ -38,10 +38,9 @@ function verifyNewUrl() {
   const express = require("express");
   const expressApp = express();
   expressApp.use(express.json());
-  const EXPRESS_PORT = 3000;
 
-  expressApp.listen(EXPRESS_PORT, () => {
-    console.log(`Express-app kjører på port ${EXPRESS_PORT}`);
+  expressApp.listen(PORT, () => {
+    console.log(`Express-app kjører på port ${PORT}`);
   });
 
   expressApp.post("/slack/events", (req, res) => {
