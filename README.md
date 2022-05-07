82907b0d037fcd59b9a91235c1f1f7bbb82f41bb
Marcus <marcus.haaland@bekk.no>
2022-05-07 09:10:23 +0200

    use ExpressReceiver from boltjs

--- README.md
+++ README.md
@@ -6,21 +6,27 @@ Dette vil beskrive hvordan man setter opp en Slackbot.
 
 ### Installasjon
 
-Antar at slack-app er korrekt satt opp.
+Antar at slack-app er korrekt satt opp i https://api.slack.com/apps/
 
-- Lag en `.env` fil og legg inn app credentials
-- `npm install`
 - Installer ngrok globalt `npm install ngrok -g`
+- Lag en `.env` fil og legg inn app credentials
+
+```
+# .env
+# Hent fra fanen "Basic Information"
+SLACK_SIGNING_SECRET=<signing-secret>
+# Hent fra fanen "OAuth & Permissions"
+SLACK_BOT_TOKEN=<xoxb-...>
+```
+
+- `npm install`
 
 ### Verifiser URL
 
-- Sett flagget `VERIFY_NEW_URL` til `true`
-  `npm start`
+- `npm start`
 - I annen terminal:
   `ngrok http 3000`
 - Hent ut url fra ngrok og verifiser url i Event Subscriptions i Slack-apps.
-- Sett flagget `VERIFY_NEW_URL` til `false`.
-- `npm start`.
 
 ## Andre kjøring
 
--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 require("dotenv").config();
-const { App } = require("@slack/bolt");
-const VERIFY_NEW_URL = false;
+const { App, ExpressReceiver } = require("@slack/bolt");
 
-/*
-  For lokal kjøring med ngrok lages en url som peker på en port. Porten brukt i bolt-app 
-  må derfor være lik den som ble satt opp med ngrok.
-*/
 const PORT = process.env.PORT || 3000;
 
-if (VERIFY_NEW_URL) {
-  verifyNewUrl();
-} else {
-  // Boten trenger noen hemmelige credentials for å få tilgang til Slack API.
-  const app = new App({
-    signingSecret: process.env.SLACK_SIGNING_SECRET, // Hent SigningSecret fra Basic Information
-    token: process.env.SLACK_BOT_TOKEN, // Hent Bot User OAuth Token fra OAuth & Permissions
-  });
+// Receiver gir adgang til den underliggende express-appen i bolt
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
 
-  (async () => {
-    await app.start(PORT);
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver,
+});
 
-    console.log(`Bolt-app kjører på port ${PORT}`);
-  })();
-
-  // Boten lytter til eventet melding med innholdet "heisann"
-  app.message("heisann", async ({ message, say }) => {
-    // say() sender en melding til kanalen hvor eventet ble avfyrt.
-    await say(`Halla på 'ræ <@${message.user}>!`);
-  });
-}
+(async () => {
+  await app.start(PORT);
+  console.log(`Bolt-app kjører på port ${PORT}`);
+})();
 
 /*
   Slack trenger å verifisere at du er eier av URL-en. Dette trengs bare å sjekkes
   en gang per nye URL, og gjøres via en POST-request.
 */
-function verifyNewUrl() {
-  const express = require("express");
-  const expressApp = express();
-  expressApp.use(express.json());
+receiver.router.post("/slack/events", (req, res) => {
+  if (req?.body?.challenge) res.send({ challenge });
+});
 
-  expressApp.listen(PORT, () => {
-    console.log(`Express-app kjører på port ${PORT}`);
-  });
-
-  expressApp.post("/slack/events", (req, res) => {
-    const { challenge } = req?.body;
-
-    if (challenge) res.send({ challenge });
-  });
-}
+// Boten lytter til eventet melding med innholdet "heisann"
+app.message("heisann", async ({ message, say }) => {
+  // say() sender en melding til kanalen hvor eventet ble avfyrt.
+  await say(`Halla på 'ræ <@${message.user}>!`);
+});
